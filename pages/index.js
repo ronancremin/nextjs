4c43f578572a54889f928b8eca1aaea9d3043c65
ronancremin <ronan@cremin.com>
2021-05-14 09:39:03 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Ronan's Next App</title>
+        <title>Create Vanya's Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
